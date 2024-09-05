1e6a14b47b75273b67f81e18ba1be0284cebc8a4
manueligno78 <manuelpintaldi@gmail.com>
2024-09-05 19:22:28 +0200

    Step for restassured

---
+++ src/test/java/steps/restassured/Step.java
@@ -0,0 +1,16 @@
+package steps.restassured;
+
+
+
+
+public class Step {
+  
+    public static void setUp() {
+        // setup restassured
+    }
+
+
+    public static void tearDown() {
+        // teardown restassured
+    }
+}
