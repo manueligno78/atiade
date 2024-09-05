3cc6bcf0fcbe5260e20a8fa55c625ab3a1167e2a
manueligno78 <manuelpintaldi@gmail.com>
2024-09-05 19:22:05 +0200

    Clean Hooks

--- src/test/java/steps/cucumber/Hooks.java
+++ src/test/java/steps/cucumber/Hooks.java
@@ -2,14 +2,10 @@ package steps.cucumber;
 
 import java.net.MalformedURLException;
 
-import org.openqa.selenium.remote.RemoteWebDriver;
-
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
 public class Hooks extends Step {
-    
-    private RemoteWebDriver driver;
 
     @Before
     public void before() throws MalformedURLException {
