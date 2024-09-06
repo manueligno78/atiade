55f7963954eeb2107a3cb61703e27fd0a38254df
manueligno78 <manuelpintaldi@gmail.com>
2024-09-06 17:33:41 +0200

    Fix browser creation for Seleniumgrid/Local

--- src/test/java/steps/playwright/PlaywrightManager.java
+++ src/test/java/steps/playwright/PlaywrightManager.java
@@ -5,10 +5,13 @@ import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import org.testng.Assert;
 
 public class PlaywrightManager {
     private static Playwright playwright;
     private static Browser browser;
+    static String browserSelected = System.getenv("BROWSER");
+    static String hubHost = System.getenv("HUB_HOST");
     private static BrowserContext context;
     private static Page page;
 
@@ -17,9 +20,28 @@ public class PlaywrightManager {
             playwright = Playwright.create();
         }
         if (browser == null) {
-            // Set headless to false to see the browser in action
-            //browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-            browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            if (browserSelected == null) {
+                browserSelected = "chrome-local";
+            }
+            if (hubHost == null) {
+                hubHost = "localhost";
+            }
+            if(browserSelected.equalsIgnoreCase("firefox")) {
+                //browser = playwright.firefox().connect("http://" + hubHost + ":4444/wd/hub");;
+                System.err.println("Firefox on Selenium Grid is not supported in Playwright");
+                Assert.assertFalse(true, "Firefox on Selenium Grid is not supported in Playwright");
+            } else if (browserSelected.equalsIgnoreCase("chrome")) {
+                //browser = playwright.chromium().connect("http://" + hubHost + ":4444/wd/hub");
+                System.err.println("Chrome on Selenium Grid is not supported in Playwright");
+                Assert.assertFalse(true, "Chrome on Selenium Grid is not supported in Playwright");
+            } else if (browserSelected.equalsIgnoreCase("firefox-local")) {
+                browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            } else if (browserSelected.equalsIgnoreCase("chrome-local")) {
+                browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            }
+            else {
+                throw new IllegalArgumentException("Browser not supported: " + browser);
+            }
         }
         return browser;
     }
