b7c99728f4313a4ff9c3d6e77dccf6954f57925c
manueligno78 <manuelpintaldi@gmail.com>
2024-09-06 22:56:57 +0200

    PageObject reviewed on CA tests

--- src/test/java/steps/cucumber/CAHome.java
+++ src/test/java/steps/cucumber/CAHome.java
@@ -3,7 +3,7 @@ package steps.cucumber;
 import java.net.MalformedURLException;
 
 import io.qameta.allure.Allure;
-import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
 import io.cucumber.java.After;
@@ -19,7 +19,7 @@ public class CAHome extends Step {
 
     @Given("I am on the Cloud Academy home page")
     public void i_am_on_the_cloud_academy_home_page() {
-        caHomePage = new CAHomePage(driver,wait);
+        caHomePage = new CAHomePage(driver, wait);
         caHomePage.get();
     }
 
@@ -28,12 +28,12 @@ public class CAHome extends Step {
         Allure.parameter("query", query);
         caHomePage.search(query);
     }
-    
+
     @Then("I should see a list of courses related to {string}")
     public void i_should_see_a_list_of_courses_related_to(String query) {
         Allure.parameter("query", query);
-        wait.until(driver -> !driver.findElements(By.xpath("//h1[@data-cy='search-result-title']")).isEmpty());
-        Assert.assertFalse(driver.findElements(By.xpath("//h1[@data-cy='search-result-title'][contains(text(),'" + query + "')]")).isEmpty());
+        wait.until(driver -> !caHomePage.getSearchResultTitles().isEmpty());
+        Assert.assertFalse(caHomePage.getSearchResultTitles().stream().noneMatch(element -> element.getText().contains(query)));
     }
 
     @Before
@@ -58,7 +58,7 @@ public class CAHome extends Step {
 
     @Then("all pricing plans should be visible")
     public void allPricingPlansShouldBeVisible() {
-        wait.until(driver -> !driver.findElements(By.xpath("//div[@data-cy='GridCol']//span[@data-cy='button-label']")).isEmpty());
+        wait.until(driver -> !caHomePage.getPricingPlans().isEmpty());
     }
 
     @When("I click on the Start Now button for the Small Teams plan")
@@ -73,10 +73,10 @@ public class CAHome extends Step {
 
     @And("I should see all the required fields to fill in the form")
     public void iShouldSeeAllTheRequiredFieldsToFillInTheForm() {
-        wait.until(driver -> !driver.findElements(By.xpath("//input[@name='name']")).isEmpty());
-        wait.until(driver -> !driver.findElements(By.xpath("//input[@name='email']")).isEmpty());
-        wait.until(driver -> !driver.findElements(By.xpath("//input[@name='password']")).isEmpty());
-        wait.until(driver -> !driver.findElements(By.xpath("//input[@name='company']")).isEmpty());
-        wait.until(driver -> !driver.findElements(By.xpath("//input[@name='phone']")).isEmpty());
+        wait.until(driver -> caHomePage.getNameField().isDisplayed());
+        wait.until(driver -> caHomePage.getEmailField().isDisplayed());
+        wait.until(driver -> caHomePage.getPasswordField().isDisplayed());
+        wait.until(driver -> caHomePage.getCompanyField().isDisplayed());
+        wait.until(driver -> caHomePage.getPhoneField().isDisplayed());
     }
 }
